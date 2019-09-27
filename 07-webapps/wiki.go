2031ce56b3c3ed85619cd2e3c10adf5a987c8769
Jeff Daube <daubejb@gmail.com>
2019-09-27 08:40:35 -0400

    implement url validation

--- 07-webapps/wiki.go
+++ 07-webapps/wiki.go
@@ -5,6 +5,8 @@ import (
     "html/template"
     "io/ioutil"
     "net/http"
+    "regexp"
+    "errors"
 )
 
 type Page struct {
@@ -13,8 +15,21 @@ type Page struct {
 }
 
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+    m := validPath.FindStringSubmatch(r.URL.Path)
+    if m == nil {
+        http.NotFound(w, r)
+        return "", errors.New("Invalid Page Title")
+    }
+    return m[2], nil // The title is the second subexpression.
+}
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/view/"):]
+    title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
     p, err := loadPage(title)
     if err != nil {
         http.Redirect(w, r, "/edit/"+title, http.StatusFound)
@@ -24,7 +39,10 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func editHandler(w http.ResponseWriter, r *http.Request) {
-    title := r. URL.Path[len("/edit/"):]
+    title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
     p, err := loadPage(title)
     if err != nil {
         p = &Page{Title: title}
@@ -33,12 +51,15 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/save/"):]
+    title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
     body := r.FormValue("body")
     p := &Page{Title: title, Body: []byte(body)}
-    err := p.save()
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
+    er := p.save()
+    if er != nil {
+        http.Error(w, er.Error(), http.StatusInternalServerError)
         return
     }
     http.Redirect(w, r, "/view/"+title, http.StatusFound)
