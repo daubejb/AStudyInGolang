d1ad4474cfcac22954f34c5f9edd000d47d8805f
Jeff Daube <daubejb@gmail.com>
2019-09-27 08:13:24 -0400

    implement templates, edit, and view

--- 07-webapps/wiki.go
+++ 07-webapps/wiki.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-    "fmt"
     "log"
+    "html/template"
     "io/ioutil"
     "net/http"
 )
@@ -15,9 +15,22 @@ type Page struct {
 func viewHandler(w http.ResponseWriter, r *http.Request) {
     title := r.URL.Path[len("/view/"):]
     p, _ := loadPage(title)
-    fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+    renderTemplate(w, "view", p)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+    title := r. URL.Path[len("/edit/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        p = &Page{Title: title}
+    }
+    renderTemplate(w, "edit", p)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+    t, _ := template.ParseFiles(tmpl + ".html")
+    t.Execute(w, p)
+}
 func (p *Page) save() error {
     filename := p.Title + ".txt"
     return ioutil.WriteFile(filename, p.Body, 0600)
@@ -34,5 +47,7 @@ func loadPage(title string) (*Page, error) {
 
 func main() {
     http.HandleFunc("/view/", viewHandler)
+    http.HandleFunc("/edit/", editHandler)
+    // http.HandleFunc("/save/", saveHandler)
     log.Fatal(http.ListenAndServe(":8080", nil))
 }
